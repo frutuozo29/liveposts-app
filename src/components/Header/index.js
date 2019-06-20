0cdfd822b40d0acc1c3ea0b30b9f525d1f859e67
Renan Frutuozo <frutuozo29@gmail.com>
2019-06-19 23:05:06 -0300

    header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+/** @jsx jsx */
+
+import { jsx } from '@emotion/core'
+
+import { header, title, icon_title } from './styles'
+
+const Header = () => (
+  <header css={header}>
+    <h1 css={title}>LivePosts</h1>
+    <a href="https://github.com/frutuozo29/liveposts-app" target="_blank" rel="noopener noreferrer">
+      <i className="fab fa-github" css={icon_title}></i>
+    </a>
+  </header>
+)
+
+export default Header
---
+++ src/components/Header/styles.js
@@ -0,0 +1,23 @@
+import { css } from '@emotion/core'
+
+export const header = css`
+  width: 100%;
+  display: grid;
+  grid-template-columns: auto 40px;
+  grid-template-rows: auto;
+  justify-items: flex-start;
+  align-content: center;
+  border-bottom: 1px solid #FFF;
+  padding: 12px 48px;
+`
+
+export const title = css`
+  color: white;
+`
+export const icon = css`fab fa-github`
+
+export const icon_title = css`
+  font-size: 40px;
+  color: white;
+  cursor: pointer;
+`
