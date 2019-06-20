4e23bf779e85d355cfd0cccf34edafcf0620f41a
Renan Frutuozo <frutuozo29@gmail.com>
2019-06-20 15:21:40 -0300

    update api url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,3 @@
 const { NODE_ENV } = process.env
 
-export const apiBaseUrl = NODE_ENV !== 'production' ? 'http://localhost:1000' : 'https://liveposts.netlify.com'
+export const apiBaseUrl = NODE_ENV !== 'production' ? 'http://localhost:1000' : 'https://liveposts-api.herokuapp.com'
