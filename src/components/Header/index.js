dfb7884ae22f97a06e1d2c75283de4bcc3a6053e
Renan Frutuozo <frutuozo29@gmail.com>
2019-06-22 11:56:35 -0300

    tests

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import { jsx } from '@emotion/core'
 import { header, title, icon_title } from './styles'
 
 const Header = () => (
-  <header css={header}>
+  <header css={header} data-testid="header">
     <h1 css={title}>LivePosts</h1>
     <a href="https://github.com/frutuozo29/liveposts-app" target="_blank" rel="noopener noreferrer">
       <i className="fab fa-github" css={icon_title}></i>
