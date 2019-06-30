8c1d2c028020f2c1ec88a59acccef7299d8eb9ed
Renan Frutuozo <frutuozo29@gmail.com>
2019-06-30 19:57:40 -0300

    test

---
+++ src/components/Loading/.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Loading from '../Loading'
+
+describe('Loading tests', () => {
+  test('Render component', () => {
+
+    const { getByTestId } = render(<Loading />)
+
+    expect(getByTestId('loading')).toBeInTheDocument(true)
+  })
+})
--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -4,7 +4,7 @@ import { jsx } from '@emotion/core'
 
 import { wrapper, forward, backward } from './styles'
 
-const Loading = ({ testid }) => {
+const Loading = ({ testid = 'loading' }) => {
   return (
     <div data-testid={testid} css={wrapper}>
       <div css={forward}></div>
